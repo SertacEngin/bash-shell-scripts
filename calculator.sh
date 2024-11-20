12779d1c8715d574f9934b9c9f070fa1ace3d651
Sertac Engin <sertacenginn@gmail.com>
2024-11-20 21:27:31 +0000

    Added calculator

---
+++ calculator.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# this is a test change
+
+# Addition function
+x=1+2
+
+# substraction
+
+# division
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+# percentage
+
+# 
+
+=======
+# multiplication
+>>>>>>> 7521c38 (demonstrate merge)
+=======
+# multiplication
+>>>>>>> merge-example
